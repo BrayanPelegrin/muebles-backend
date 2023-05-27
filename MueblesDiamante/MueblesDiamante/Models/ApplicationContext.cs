2c254b9976caae690a425fdaa794301d5074e95a
BrayanPelegrin <peletu109@hotmail.com>
2023-05-27 15:10:31 -0400

    Agregando Base al OnModelCreating

--- MueblesDiamante/MueblesDiamante/Models/ApplicationContext.cs
+++ MueblesDiamante/MueblesDiamante/Models/ApplicationContext.cs
@@ -17,7 +17,10 @@ namespace MueblesDiamante.Models
         public DbSet<Status> Status { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+
         }
 
     }
